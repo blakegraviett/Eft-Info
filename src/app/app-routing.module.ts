6a63543559cba4d4d9ed17c8d5c8727ca4caf607
Blake Graviett <blake.graviett@gmail.com>
2024-01-22 12:02:54 -0600

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,7 +4,13 @@ import { LandingPageComponent } from './landing-page/landing-page.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
-  {path: 'home', component: LandingPageComponent}
+  {path: 'home', component: LandingPageComponent},
+  {path: 'quest', component: LandingPageComponent},
+  {path: 'hideout', component: LandingPageComponent},
+  {path: 'maps', component: LandingPageComponent},
+  {path: 'flea-market', component: LandingPageComponent},
+  {path: 'shopping-list', component: LandingPageComponent},
+
 ];
 
 @NgModule({
